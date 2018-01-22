2da2a4685ae43a2589740e8c40931027019de1c0
huangjianping <292708638@qq.com>
2018-01-22 13:12:50 +0800

    netty commit

---
+++ src/main/java/com/hjp/utils/Calculator.java
@@ -0,0 +1,13 @@
+package com.hjp.utils;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public enum Calculator {
+    Instance;
+    private final static ScriptEngine jse = new ScriptEngineManager().getEngineByName("JavaScript");
+    public Object cal(String expression) throws ScriptException {
+        return jse.eval(expression);
+    }
+}
